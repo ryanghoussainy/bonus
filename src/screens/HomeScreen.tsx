a85f880585973c8ef4ac14833202acdb3b3ee44f
Ryan Ghoussainy <rg1523@ic.ac.uk>
2024-08-22 17:27:35 +0100

    Fixed Home Screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,59 +1,87 @@
-import { useCallback, useState } from 'react'
-import { FlatList, View, ActivityIndicator } from 'react-native'
-import { UserDeal_t, getUserDeals } from '../operations/UserDeal'
-import Deal from '../components/Deal'
-import Colours from '../config/Colours'
-import { Session } from '@supabase/supabase-js'
-import { Button, Text } from '@rneui/themed'
-import { useFocusEffect } from '@react-navigation/native'
-import { LinearGradient } from 'expo-linear-gradient'
-import { useTheme } from '../contexts/ThemeContext'
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import { Session } from '@supabase/supabase-js';
+import { useCallback, useState } from 'react';
+import { Button } from '@rneui/themed';
+import Colours from '../config/Colours';
+import { getUserDeals, UserDeal_t } from '../operations/UserDeal';
+import Deal from '../components/Deal';
+import { useFocusEffect } from '@react-navigation/native';
+import Fonts from '../config/Fonts';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useTheme } from '../contexts/ThemeContext';
 
 export default function HomeScreen({ session }: { session: Session }) {
-  const [deals, setDeals] = useState<UserDeal_t[]>([])
-  const [loading, setLoading] = useState(true)
-  const { theme } = useTheme() // Get the current theme
+    // Get theme
+    const { theme } = useTheme();
 
-  const fetchDeals = async () => {
-    setLoading(true)
-    await getUserDeals(session, setDeals)
-    setLoading(false)
-  }
+    const [loading, setLoading] = useState<boolean>(true)
 
-  // Focus Effect to fetch deals when the screen is focused
-  useFocusEffect(useCallback(() => {
-    fetchDeals()
-  }, [session]))
+    const [deals, setDeals] = useState<UserDeal_t[]>([])
 
-  return (
-    <LinearGradient
-      style={{ flex: 1 }}
-      colors={[Colours.background[theme], Colours.dealItem[theme]]}
-    >
-      <FlatList
-        data={deals}
-        renderItem={({ item }) => <Deal session={session} deal={item} />}
-        ListEmptyComponent={() => {
-          if (loading) {
-            return (
-              <ActivityIndicator size="large" color={Colours.primary} />
-            )
-          } else {
-            return (
-              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ fontSize: 18, fontWeight: '500', color: Colours.text[theme], textAlign: 'center' }}>
-                  No deals found.
-                </Text>
-                <Button
-                  title="Refresh"
-                  onPress={fetchDeals}
-                  color={Colours.primary}
-                />
-              </View>
-            )
-          }
-        }}
-      />
-    </LinearGradient>
-  )
+    const fetchDeals = async () => {
+        setLoading(true);
+        await getUserDeals(session, setDeals);
+        setLoading(false);
+    };
+
+    // Focus Effect to fetch deals when the screen is focused
+    useFocusEffect(useCallback(() => {
+        fetchDeals()
+    }, [session]))
+
+    return (
+        <LinearGradient style={{ flex: 1 }} colors={[Colours.background[theme], Colours.dealItem[theme]]}>
+            <View style={[styles.headerContainer, { backgroundColor: Colours.background[theme] }]}>
+                <Text style={[styles.header, { color: Colours.text[theme] }]}>Your Deals</Text>
+            </View>
+            <FlatList
+                data={deals}
+                renderItem={({ item }) => <Deal session={session} deal={item} />}
+                keyExtractor={(item) => item.id}
+                ListEmptyComponent={() => {
+                    if (loading) {
+                        return (
+                            <View style={[styles.container, { backgroundColor: Colours.background[theme] }]}>
+                                <ActivityIndicator size="large" color={Colours.primary} />
+                            </View>
+                        )
+                    } else {
+                        return (
+                            <View style={[styles.container, { backgroundColor: Colours.background[theme] }]}>
+                                <Text style={[styles.text, { color: Colours.text[theme] }]}>No deals found.</Text>
+                                <Button
+                                    title="Refresh"
+                                    onPress={fetchDeals}
+                                    color={Colours.primary}
+                                />
+                            </View>
+                        )
+                    }
+                }}
+            />
+        </LinearGradient>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "500",
+        textAlign: "center",
+    },
+    headerContainer: {
+        marginTop: 20,
+        padding: 15,
+    },
+    header: {
+        fontSize: 28,
+        fontWeight: "bold",
+        fontFamily: Fonts.condensed,
+        marginLeft: 10,
+    },
+})
